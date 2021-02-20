f4b09cbc05cf672ae6f41ef22491e551d2eaf598
Oleksii Riahuzov <riahuzovoleksii@gmail.com>
2021-02-20 16:24:44 +0200

    Add HW3 implementation.

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -9,3 +10,11 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals(date_time, description, calories, user_id)
+VALUES ('2021-02-20 12:00', 'Обед', 1000, 100000),
+       ('2021-02-20 18:00', 'Ужин', 250, 100001),
+       ('2021-02-20 08:00', 'Завтрак', 200, 100000),
+       ('2021-02-20 08:30', 'Завтрак', 200, 100001),
+       ('2021-02-20 10:00', 'Ужин', 200, 100000),
+       ('2021-02-20 16:00', 'Полдник', 200, 100001);
