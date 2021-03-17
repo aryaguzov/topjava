3e69037284096a87a8351cc353d8ad6581df1ff1
Oleksii Riahuzov <riahuzovoleksii@gmail.com>
2021-03-17 20:49:17 +0200

    Add HW6 implementation.

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -0,0 +1,74 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.service.MealService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
+
+@RequestMapping("/meals")
+@Controller
+public class JspMealController extends AbstractMealRestController {
+
+    public JspMealController(MealService service) {
+        super(service);
+    }
+
+    @GetMapping
+    public String getAll(Model model) {
+        model.addAttribute("meals", super.getAll());
+        return "meals";
+    }
+
+    @GetMapping("/update")
+    public String updateOrCreate(Model model, HttpServletRequest request) {
+        model.addAttribute("meal", StringUtils.hasText(request.getParameter("id")) ?
+                super.get(getId(request)) :
+                new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 0));
+        return "mealForm";
+    }
+
+    @GetMapping("/delete")
+    public String delete(HttpServletRequest request) {
+        super.delete(getId(request));
+        return "redirect:/meals";
+    }
+
+    @PostMapping
+    public String save(HttpServletRequest request) {
+        Meal meal = new Meal(
+                LocalDateTime.parse(request.getParameter("dateTime")),
+                request.getParameter("description"),
+                Integer.parseInt(request.getParameter("calories")));
+        if (StringUtils.hasLength(request.getParameter("id"))) {
+            super.update(meal, getId(request));
+        } else {
+            super.create(meal);
+        }
+        return "redirect:/meals";
+    }
+
+    @GetMapping("/filter")
+    public String getBetween(HttpServletRequest request, Model model) {
+        model.addAttribute("meals", super.getBetween(
+                parseLocalDate(request.getParameter("startDate")),
+                parseLocalTime(request.getParameter("startTime")),
+                parseLocalDate(request.getParameter("endDate")),
+                parseLocalTime(request.getParameter("endTime"))));
+        return "meals";
+    }
+
+    private int getId(HttpServletRequest request) {
+        String paramId = Objects.requireNonNull(request.getParameter("id"));
+        return Integer.parseInt(paramId);
+    }
+}
--- src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
@@ -5,6 +5,8 @@ import org.junit.Rule;
 import org.junit.rules.ExternalResource;
 import org.junit.rules.Stopwatch;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
@@ -12,6 +14,9 @@ import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.ActiveDbProfileResolver;
 import ru.javawebinar.topjava.TimingRules;
+
+import static org.springframework.core.env.Profiles.*;
+import static ru.javawebinar.topjava.Profiles.*;
 
 import static org.junit.Assert.assertThrows;
 import static ru.javawebinar.topjava.util.ValidationUtil.getRootCause;
@@ -28,6 +33,9 @@ abstract public class AbstractServiceTest {
     @ClassRule
     public static ExternalResource summary = TimingRules.SUMMARY;
 
+    @Autowired
+    Environment environment;
+
     @Rule
     public Stopwatch stopwatch = TimingRules.STOPWATCH;
 
@@ -40,5 +48,9 @@ abstract public class AbstractServiceTest {
                 throw getRootCause(e);
             }
         });
+    }
+
+    public boolean isNotJdbc() {
+        return environment.acceptsProfiles(of(JPA, DATAJPA));
     }
 }
