1d350151ee25f8c681a95c74e37238457d56dc65
Oleksii Riahuzov <riahuzovoleksii@gmail.com>
2021-04-08 16:52:59 +0300

    9 06 validation

--- src/main/java/ru/javawebinar/topjava/to/UserTo.java
+++ src/main/java/ru/javawebinar/topjava/to/UserTo.java
@@ -6,10 +6,17 @@ import javax.validation.constraints.Size;
 
 public class UserTo extends BaseTo {
 
+    @NotBlank
+    @Size(min = 2, max = 100)
     private String name;
 
+    @Email
+    @NotBlank
+    @Size(max = 100)
     private String email;
 
+    @NotBlank
+    @Size(min = 5, max = 32, message = "length must be between 5 and 32 characters")
     private String password;
 
     public UserTo() {
