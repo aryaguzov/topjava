348779e93974d2fb35294b96c71628062d910cbe
Oleksii Riahuzov <riahuzovoleksii@gmail.com>
2021-04-08 16:52:40 +0300

    9 05 update

--- src/main/java/ru/javawebinar/topjava/to/UserTo.java
+++ src/main/java/ru/javawebinar/topjava/to/UserTo.java
@@ -1,5 +1,9 @@
 package ru.javawebinar.topjava.to;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class UserTo extends BaseTo {
 
     private String name;
