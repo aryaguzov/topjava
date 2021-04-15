10647b6816b7cb9a66828b09f869bb159997fdf1
Oleksii Riahuzov <riahuzovoleksii@gmail.com>
2021-04-15 20:09:45 +0300

    10 09 registration

--- src/test/java/ru/javawebinar/topjava/web/json/JsonUtilTest.java
+++ src/test/java/ru/javawebinar/topjava/web/json/JsonUtilTest.java
@@ -1,15 +1,17 @@
 package ru.javawebinar.topjava.web.json;
 
-import com.fasterxml.jackson.databind.ObjectWriter;
 import org.junit.jupiter.api.Test;
 import ru.javawebinar.topjava.MealTestData;
-import ru.javawebinar.topjava.View;
+import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static ru.javawebinar.topjava.MealTestData.*;
 
 class JsonUtilTest {
@@ -31,10 +33,13 @@ class JsonUtilTest {
     }
 
     @Test
-    public void writeWithView()  {
-        ObjectWriter uiWriter = JacksonObjectMapper.getMapper().writerWithView(View.JsonUI.class);
-        String json = JsonUtil.writeValue(adminMeal1, uiWriter);
+    void writeOnlyAccess() {
+        String json = JsonUtil.writeValue(UserTestData.user);
         System.out.println(json);
-        assertThat(json, containsString("dateTimeUI"));
+        assertThat(json, not(containsString("password")));
+        String jsonWithPass = UserTestData.jsonWithPassword(UserTestData.user, "newPass");
+        System.out.println(jsonWithPass);
+        User user = JsonUtil.readValue(jsonWithPass, User.class);
+        assertEquals(user.getPassword(), "newPass");
     }
 }
