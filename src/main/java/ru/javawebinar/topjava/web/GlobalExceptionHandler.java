83824a2095dfb67432a3ee11f4fcd59147610d0a
Oleksii Riahuzov <riahuzovoleksii@gmail.com>
2021-04-22 16:30:51 +0300

    11 08 i18n

--- src/main/java/ru/javawebinar/topjava/web/GlobalExceptionHandler.java
+++ src/main/java/ru/javawebinar/topjava/web/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,6 +17,12 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
     private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    private final MessageSourceAccessor messageSourceAccessor;
+
+    public GlobalExceptionHandler(MessageSourceAccessor messageSourceAccessor) {
+        this.messageSourceAccessor = messageSourceAccessor;
+    }
 
     @ExceptionHandler(Exception.class)
     public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
